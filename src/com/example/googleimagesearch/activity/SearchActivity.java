992d3e9a24f875e407e97b38c3fa8b323cbdd7ed
Anand Biligiri <joywhitehill-github@yahoo.com>
2014-01-15 00:57:44 -0800

    Initial commit, volley integrated

---
+++ src/com/example/googleimagesearch/activity/SearchActivity.java
@@ -0,0 +1,24 @@
+package com.example.googleimagesearch.activity;
+
+import com.example.googleimagesearch.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SearchActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.search, menu);
+		return true;
+	}
+
+}
